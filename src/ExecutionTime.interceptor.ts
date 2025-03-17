31bdd83b64400b5b8ab3bad154a8d96dd932d134
Rosintan Sitinjak <2381014@unai.edu>
2025-03-17 21:40:14 +0700

    Push handson-7 for handson-8 activity

---
+++ src/ExecutionTime.interceptor.ts
@@ -0,0 +1,31 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Logger,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable, tap } from 'rxjs';
+
+export class ExecutionTime implements NestInterceptor {
+  private readonly logger = new Logger(ExecutionTime.name);
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    const handler = context.getHandler();
+    const methodName = handler.name;
+    const className = context.getClass().name;
+
+    this.logger.log(`Before... calling ${className}.${methodName}`);
+    const startTime = Date.now();
+
+    return next.handle().pipe(
+      tap(() => {
+        // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+        this.logger.log(
+          `After... ${className}.${methodName} took ${Date.now() - startTime}ms`,
+        );
+      }),
+    );
+  }
+}
--- src/songs/create-song.dto.ts
+++ src/songs/create-song.dto.ts
@@ -1,4 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { IsString } from 'class-validator';
+
 export class CreateSongDTO /*Data Transfer Object*/ {
+  @IsString()
   title: string;
+  @IsString()
   artist: string;
 }
